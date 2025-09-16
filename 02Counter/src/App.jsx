57576da29b4a271e0c98d6f44dd8ff36360a1cf7
khotAkash07 <khotakash5737@gmail.com>
2025-09-16 22:25:43 +0530

    This is counter project

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Counter from "./js/conter";
+
+function App() {
+  return (
+    <>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
