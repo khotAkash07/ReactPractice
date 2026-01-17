2f18fd394f11172dc6b4413e2428a55bcfadc569
khotAkash07 <khotakash5737@gmail.com>
2026-01-17 10:30:00 +0530

    UnControlledComponanats

--- FromJan26/src/UncontrolledComponants/UncontrolledComponantDemo.jsx
+++ FromJan26/src/UncontrolledComponants/UncontrolledComponantDemo.jsx
@@ -11,6 +11,7 @@ let UncontrolledComponentDemo=()=>{
             <form action="" onSubmit={handleForm} className="mx-52"><br/>
 
                 <input type="text" name="firstName" id="fname" placeholder="First Name" className="bg-black h-10 w-52 mb-4 text-white p-2"/><br/>
+
                 <input type="text" name="lName" id="lname" placeholder="Last Name" className="bg-black h-10 w-52 text-white p-2"/><br/>
                 <button className="bg-yellow-300 p-2 mt-2" >Register</button>
             </form>
