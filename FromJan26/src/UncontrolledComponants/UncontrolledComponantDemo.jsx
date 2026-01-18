f7510150f8e03b56a3f00f32af61c66637fd59ec
khotAkash07 <khotakash5737@gmail.com>
2026-01-18 10:30:00 +0530

    UnControlledComponanats

--- FromJan26/src/UncontrolledComponants/UncontrolledComponantDemo.jsx
+++ FromJan26/src/UncontrolledComponants/UncontrolledComponantDemo.jsx
@@ -2,18 +2,19 @@ let UncontrolledComponentDemo=()=>{
 
     function handleForm(event){
         event.preventDefault();
-        console.log("handleForm");
+        let fname = document.querySelector("#fname").value;
+        let lname = document.querySelector("#lname").value;
+        console.log("handleForm", fname, lname);
     }
     return(
         <div>
-            <form action="" onSubmit={handleForm}><br/>
-                <input type="text" name="firstName" id="fname" placeholder="First Name"/><br/>
-                <input type="text" name="lName" id="lname" placeholder="Last Name"/><br/>
-                <button>Register Here</button>
+            <form action="" onSubmit={handleForm} className="mx-52"><br/>
+                <input type="text" name="firstName" id="fname" placeholder="First Name" className="bg-black h-10 w-52 mb-4 text-white p-2"/><br/>
+                <input type="text" name="lName" id="lname" placeholder="Last Name" className="bg-black h-10 w-52 text-white p-2"/><br/>
+                <button className="bg-yellow-300 p-2 mt-2" >Register</button>
             </form>
         </div>
     );
-
 }
 
 export default UncontrolledComponentDemo;
